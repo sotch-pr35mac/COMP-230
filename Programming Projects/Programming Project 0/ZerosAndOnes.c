89cf0b81840d18012ed5c621e9e4abc18bf92379
Preston Stosur-Bassett <preston@stosur.info>
2015-04-01 09:42:44 -0400

    added some to programming project 0

---
+++ Programming Projects/Programming Project 0/ZerosAndOnes.c
@@ -0,0 +1,33 @@
+/*
+  Author: Preston Stosur-Bassett
+  Date: 1.4.15
+  Description: This is no better than a hello world.
+*/
+
+#include<stdio.h>
+
+main() {
+  int zeroCounter = 0;
+  int oneCounter = 0;
+  int i = 0;
+  char currentChar;
+
+  while(i < 32) {
+    printf("Please enter a number: ");
+    scanf("%c", &currentChar);
+    printf(" \n");
+
+    if(currentChar == '0') {
+      zeroCounter++;
+    }
+    else if(currentChar == '1') {
+      oneCounter++;
+    }
+
+    i++;
+  }
+
+  printf("You had %i 0's and %i 1's.\n", zeroCounter, oneCounter);
+
+  printf("Program Complete.\n");
+}
