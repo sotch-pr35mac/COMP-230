529e453eee640cb8e44ef035f9daa6f603463a9a
Preston Stosur-Bassett <preston@stosur.info>
2015-05-16 19:28:59 -0400

    so the segfault error is coming from the inst variable i believe

--- Programming Projects/Programming Project 3/Code/main.c
+++ Programming Projects/Programming Project 3/Code/main.c
@@ -58,13 +58,34 @@ int main(int argc, char *argv[]) {
 
   /* Continuously read next line of input until End Of File is reached. */
   for(PC = 0; fgets(inst, BUFSIZ, fptr); PC += 4) {
+
+    printf("DEBUG: Test 18\n");
+
     /* Remove the comments */
-    if(*inst == '#') continue;
+    if(*inst == '#') {
+
+      printf("DEBUG: Test 30\n");
+
+      continue;
+
+      printf("DEBUG: Test 31\n");
+    }
+
+    printf("DEBUG: Test 31\n");
+
+    printf("The value of inst is: %s\n", inst);
+
+    printf("DEBUG: Test 31.5");
+
     (void) strtok (inst, (char* )'#');
+
+    printf("DEBUG: Test 19\n");
 
     /* point the token to the first non-white space character. */
     superTokenBegin = inst;
     getToken(&superTokenBegin, &superTokenEnd);
+
+    printf("DEBUG: Test 20\n");
 
     /* skip over labels */
     if(*(superTokenEnd) == ':') {
@@ -72,26 +93,38 @@ int main(int argc, char *argv[]) {
       getToken(&superTokenBegin, &superTokenEnd);
     }
 
+    printf("DEBUG: Test 21\n");
+
     /* Get operation */
     *superTokenEnd = '\0';
     operation = superTokenBegin;
+
+    printf("DEBUG: Test 22\n");
 
     /* Get operation format */
     char * opCode = getOpCode(operation);
     int format = getFormat(opCode);
 
+    printf("DEBUG: Test 23\n");
+
     /* Parse the instruction */
     if(format == 0) {
+      printf("DEBUG: Test 24\n");
       /* Handle R format instructions */
       parseR(inst, opCode, operation, &superTokenBegin, &superTokenEnd, lineNum);
+      printf("DEBUG: Test 25\n");
     }
     else if(format == 1) {
+      printf("DEBUG: Test 26\n");
       /* Handle I type instructions */
       parseI(inst, opCode, &superTokenBegin, &superTokenEnd, lineNum, tableOfLabels);
+      printf("DEBUG: Test 27\n");
     }
     else if(format == 2) {
+      printf("DEBUG: Test 28\n");
       /* Handle J type instructions */
       parseJ(inst, opCode, &superTokenBegin, &superTokenEnd, lineNum, tableOfLabels);
+      printf("DEBUG: Test 29\n");
     }
 
     lineNum++;
